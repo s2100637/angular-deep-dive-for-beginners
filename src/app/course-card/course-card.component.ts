026f951d2d6e1bbf8d664efa0143d7d0ec8762ab
s2100637 <peter.knutas@edu.bc.fi>
2022-02-16 13:55:30 +0200

    Tilanne osien 7. 8. sekä 9. jälkeen

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { COURSES } from 'src/db-data';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course:Course;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
